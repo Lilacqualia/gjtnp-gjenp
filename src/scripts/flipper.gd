8b723eea38e1fdf870bfba20efcc318f02a52474
tomorrowsjelly <incrediblefrown@gmail.com>
2026-02-16 15:14:24 -0800

    initial commit
    prototype flippers and ball

---
+++ src/scripts/flipper.gd
@@ -0,0 +1,52 @@
+extends Node2D
+
+enum Flipper_Direction { 
+	## left flipper activated by a and z
+	LEFT,
+	## right flipper activated by d and /
+	RIGHT 
+}
+
+## resting state while untriggered. default value is 0. be aware that flipper appears at 45 degrees, not 0, in the editor
+@export var min_rotation := 0.0
+
+## resting state while triggered. default value is 45. be aware that flipper seems to overshoot this angle slightly and snap back, presumably to emulate a physical object
+@export var max_rotation := 45.0
+
+## speed at which flipper moves between states. default value is 3
+@export var base_acceleration := 3.0
+
+## changes the triggering input *only*. use inspector -> node2d -> transform -> scale and set x to -1 to flip the actual object
+@export var flipper_direction : Flipper_Direction
+
+var flipping_up := false
+var acceleration = 0.0
+
+func _physics_process(delta: float) -> void:
+	if flipper_direction == 0: #                           maps flipper to its left/right key. feels a little hacky, redo if it starts losing inputs
+		if Input.is_action_just_pressed("Left Flipper"):
+			flipping_up = true
+		elif Input.is_action_just_released("Left Flipper"):
+			flipping_up = false
+	elif flipper_direction == 1:
+		if Input.is_action_just_pressed("Right Flipper"):
+			flipping_up = true
+		elif Input.is_action_just_released("Right Flipper"):
+			flipping_up = false
+	
+	if flipping_up:
+		if $AnimatableBody2D.rotation > deg_to_rad(min_rotation) + 0.01:
+			if acceleration == 0:
+				acceleration = 2 * base_acceleration
+			$AnimatableBody2D.rotation -= acceleration * delta
+			acceleration += base_acceleration
+		else:
+			$AnimatableBody2D.rotation = deg_to_rad(min_rotation)
+			acceleration = 0.0
+	else:
+		if $AnimatableBody2D.rotation < deg_to_rad(max_rotation) - 0.01:
+			$AnimatableBody2D.rotation += acceleration * delta
+			acceleration = base_acceleration * 4
+		else:
+			$AnimatableBody2D.rotation = deg_to_rad(max_rotation)
+			acceleration = 0.0
---
+++ src/scripts/pinball.gd
@@ -0,0 +1,5 @@
+extends RigidBody2D
+
+
+func _process(_delta: float) -> void: # prevents gradient from rotating with ball for faux shine
+	$gradient.rotation = -rotation
