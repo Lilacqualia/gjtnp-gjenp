7f399f2ffdd43accb0eec6bc5d2447201a190663
mint-cookie <mint.makes.glass@gmail.com>
2026-02-19 19:42:23 -0500

    Add drop targets
    A drop target is a hittable object, which means something interesting
    happens when the pinball hits it. After it gets hit, it changes in
    appearance and deactivates its collision box.

--- src/scripts/pinball.gd
+++ src/scripts/pinball.gd
@@ -10,10 +10,7 @@ func _on_body_entered(body: Node) -> void:
 	pass # Replace with function body.
 	#if body:
 		#print("ball thinks it hit something hittable")
-	emit_signal("ball_hit_something", body)
+	if(body.is_in_group("hittable_object")):
+		emit_signal("ball_hit_something", body)
 	#else:
 		
-
-
-func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
-	pass # Replace with function body.
--- src/scripts/test_table.gd
+++ src/scripts/test_table.gd
@@ -8,12 +8,13 @@ func _on_drain_killbox_body_entered(body: Node2D) -> void: # cribbed from the sa
 	if body.is_in_group("pinballs"):
 		body.remove_from_group("pinballs")
 		await get_tree().create_timer(1).timeout
+		body.get_signal_list()
 		body.queue_free()
 		var new_ball : Node2D = PinballScene.instantiate()
 		new_ball.position = pinball_spawn_point
+		# reconnect the signal, since this is a new instance, and the signal isn't defined on the class
+		new_ball.connect("ball_hit_something", _on_pinball_hit)
 		add_child.call_deferred(new_ball)
 	
 func _on_pinball_hit(hit_object: Node) -> void:
-	print("table thinks you hit %s" % hit_object.name)
-	if hit_object.has_method("receive_hit"):
-		hit_object.receive_hit()
+	hit_object.receive_hit()
