7d18872d74b89c621b6c7f605f8986faa1be6f42
tomorrowsjelly <incrediblefrown@gmail.com>
2026-02-22 10:51:14 -0800

    spinner wip 1
    spins but doesn't contact the board

---
+++ src/scripts/spinner_area2d.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+## point value per spin
+@export var value: int
+
+signal spinner_flipped(pointvalue)
+
+func _on_body_entered(body: Node2D) -> void: # reads the speed of the pinball, determines how many times to spin, signals the board to add points
+	var momentum: Vector2 = body.linear_velocity
+	var speed: float = momentum.length()
+	print("speed: " + str(speed) + " mph")
+	var spins: int = speed/300
+	print("spins: " + str(spins))
+	spinner_flipped.emit(value * spins)
