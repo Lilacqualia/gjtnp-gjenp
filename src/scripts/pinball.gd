99edbcc932f2b680149eb7f14aaab7ed1c029c37
mint-cookie <mint.makes.glass@gmail.com>
2026-02-23 21:02:21 -0500

    Add SFX for drop target, flipper, and walls
    When the ball hits a drop target, make a sound. It doesn't matter how
    hard the ball hit the target.
    
    When the ball hits a wall, make a sound. The volume of the sound is
    proportional to how hard the ball hit the wall. This is calculated based
    on a difference in velocity before and after the collision.
    
    When a flipper is activated or deactivated, make a sound. The left
    flipper makes sounds in the left channel, and the right flipper makes
    sounds in the right channel.

--- src/scripts/pinball.gd
+++ src/scripts/pinball.gd
@@ -2,10 +2,19 @@ extends RigidBody2D
 
 signal ball_hit_something(hit_object: Node)
 
+var old_velocity: Vector2
+var min_velocity_for_thunk = 200000.0
+var max_velocity_for_thunk = 3000000.0
+
 func _process(_delta: float) -> void: # prevents gradient from rotating with ball for faux shine
 	$gradient.rotation = -rotation
-
+	old_velocity = linear_velocity
 
 func _on_body_entered(body: Node) -> void:
 	if(body.is_in_group("hittable_object")):
 		emit_signal("ball_hit_something", body)
+	elif(body.is_in_group("walls")):
+		var velocity_shift = old_velocity.distance_squared_to(linear_velocity)
+		if velocity_shift >= min_velocity_for_thunk:
+			$Thunk.volume_linear = (minf(velocity_shift, max_velocity_for_thunk) / max_velocity_for_thunk) * 0.85
+			$Thunk.play()
