163961fd9b9fa7952a6b6d99c5a9cf3267a8a930
mint-cookie <mint.makes.glass@gmail.com>
2026-02-28 19:17:01 -0500

    remove commented-out code

--- src/scripts/table_forest.gd
+++ src/scripts/table_forest.gd
@@ -6,7 +6,6 @@ var score: int = 0
 var table_is_done = false
 var ball_resetter: Timer
 
-#signal update_score_display
 signal go_to_scene(scene: Globals.SceneName)
 signal update_message(message: String)
 
@@ -19,9 +18,6 @@ func _ready() -> void:
 	ball_resetter = Timer.new()
 	ball_resetter.one_shot = true
 	ball_resetter.timeout.connect(_reset_the_ball)
-	#ball_resetter.start(3.0)
-	#ball = PinballScene.instantiate()
-	#ball.position = ResetPosition
 	add_child.call_deferred(ball_resetter)
 	ball_resetter.start.call_deferred(3.0)
 	
@@ -54,18 +50,12 @@ func _reset_the_ball():
 		add_child.call_deferred(ball)
 		ball.set_deferred("position", ResetPosition)
 		ball.set_deferred("linear_velocity", Vector2.ZERO)
-	
-	#call_deferred()
-	#ball.position = ResetPosition
-	#ball.linear_velocity = Vector2.ZERO
-	
 
 func _on_pinball_hit(hit_object: Node) -> void:
 	hit_object.receive_hit(ball.linear_velocity)
 	if hit_object.value:
 		print("add %d points" % hit_object.value)
 		score += hit_object.value
-		#emit_signal("update_score_display", score)
 		emit_signal("update_message", str(score))
 	if score >= ScoreToWin:
 		print("you win forest table, time for cave table")
